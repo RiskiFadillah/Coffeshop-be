f428d7d22de6e3816c038a5581168bc282de70d2
RiskiFadillah <riski.fadil95@gmail.com>
2023-03-22 11:25:37 +0700

    modified:   src/model/auth.model.js

--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -7,32 +7,28 @@ const authModel = {
   login: ({ email, password }) => {
     console.log(email, password);
     return new Promise((resolve, reject) => {
-      db.query(
-        `SELECT * FROM users WHERE username=$1`,
-        [email],
-        (err, result) => {
-          if (err) {
-            return reject(err.message);
+      db.query(`SELECT * FROM users WHERE email=$1`, [email], (err, result) => {
+        if (err) {
+          return reject(err.message);
+        } else {
+          if (result.rows.length == 0) {
+            return reject("email/password salah"); //ketika username salah
           } else {
-            if (result.rows.length == 0) {
-              return reject("email/password salah"); //ketika username salah
-            } else {
-              bcrypt.compare(
-                password,
-                result.rows[0].password,
-                function (err, hashingResult) {
-                  if (err) return reject("email/password salah"); //ketika kesalaahan hashing bycrypt
-                  if (hashingResult) {
-                    return resolve(result.rows[0]);
-                  } else {
-                    return reject("email/password salah."); //ketika password salah
-                  }
+            bcrypt.compare(
+              password,
+              result.rows[0].password,
+              function (err, hashingResult) {
+                if (err) return reject("email/password salah"); //ketika kesalaahan hashing bycrypt
+                if (hashingResult) {
+                  return resolve(result.rows[0]);
+                } else {
+                  return reject("email/password salah."); //ketika password salah
                 }
-              );
-            }
+              }
+            );
           }
         }
-      );
+      });
     });
   },
   register: ({ email, password, phone_number }) => {
